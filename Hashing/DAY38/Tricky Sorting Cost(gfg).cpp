d7adf999316ca726acd132ff137d9e413c32cba2
Pragati Tomar <86148240+PragatiTomar@users.noreply.github.com>
2023-04-05 12:18:08 +0530

    Create Tricky Sorting Cost(gfg).cpp
    Given an array arr[] of N elements containing first N positive integers. You have to sort the integers in ascending order by the following operation. Operation is to pick an integer and place it at end or at start. Every such operation increases cost by one. The task is to sort the array in the minimum cost
    
    Example 1:
    
    Input: N = 3
    arr = {2, 1, 3}
    Output: 1
    Explaination: Place 1 at start.

---
+++ Hashing/DAY38/Tricky Sorting Cost(gfg).cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    int sortingCost(int N, int arr[]){
+        // code here
+          map<int,int> mp;
+        int mx =0;
+        for(int i=0;i<N;i++)
+        {
+           if(mp.find(arr[i]-1)!=mp.end())
+           mp[arr[i]] = mp[arr[i]-1];
+          
+           mp[arr[i]]++;
+           mx = max(mx ,mp[arr[i]]);
+        }
+        
+        return N-mx;
+    }
+};
